3efdf3d7a69cae3d203e88fa236705ad20060114
urry <urry.vinnitsa@gmail.com>
2024-07-08 21:23:14 +0300

    first commit

---
+++ inc/main.h
@@ -0,0 +1,20 @@
+
+//**************************************************
+//! Файл                                    : main.h
+//**************************************************
+#ifndef __MAIN_H
+#define __MAIN_H
+
+
+#include "stm32f4xx.h"
+
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "contiki.h"
+
+
+
+#endif /* __MAIN_H */
+
+
